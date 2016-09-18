54f961ef17e8376b13b32e2c655211faa88f5c59
HailsTails <Helencodemonkey@gmail.com>
2016-09-18 11:54:33 +1000

    Removed Unusable Code
    Removed code from previous project that was unapplicable

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -15,82 +15,17 @@ app.config([
           }]
         }
       })
-      .state('tasks', {
-        url: '/tasks/{id}',
-        templateUrl: '/tasks.html',
-        controller: 'TasksCtrl'
-      });
 
     $urlRouterProvider.otherwise('home');
 }]);
 
-app.factory('tasks', ['%http', function(){
-  var o = {
-    tasks: []
-  }
-  o.getAll = function(){
-    return $http.get('/tasks').success(function(data){
-      angular.copy(data, o.tasks);
-    });
-  };
-  return o;
-}])
-
-app.controller('TasksCtrl', [
-  '$scope',
-  '$stateParams',
-  'tasks',
-  function($scope, $stateParams, tasks){
-    $scope.task = tasks.tasks[$stateParams.id];
-
-    //Tick off a task (change it to done)
-    $scope.tickOff = function(task){
-      if(task.done == ' '){
-          task.done = 'x';
-      }
-      else if(task.done == 'x'){
-          task.done = ' ';
-      }
-    }
-
-    $scope.addSubTask = function(task){
-      $scope.task.sub_tasks.push({
-        title: $scope.title,
-        done: ' '
-      });
-    }
-  }
-]);
-
 app.controller('MainCtrl', [
   '$scope',
-  'tasks',
-  function($scope, tasks){
+  function($scope){
     //Bind list
-    $scope.tasks = tasks.tasks;
 
     //Add Tasks
     $scope.addTask = function(){
       console.log('blah');
-      //if(!$scope.title || $scope.title === '') { return; }
-      $scope.tasks.push({
-        title: $scope.title,
-        due_date: $scope.due_date,
-        done: ' ',
-        sub_tasks: [
-          {title: 'try', done: 'x'},
-          {title: 'try harder', done: ' '}
-        ]
-      });
-    }
-
-    //Tick off a task (change it to done)
-    $scope.tickOff = function(task){
-      if(task.done == ' '){
-          task.done = 'x';
-      }
-      else if(task.done == 'x'){
-          task.done = ' ';
-      }
     }
 }]);
